366621e98d9a8983df9a2f70ebe7d8aaed156bcd
Allan Thrane Andersen <einheriii@gmail.com>
2025-08-04 17:24:26 +0200

    main.js fails wit error: Cannot find module '...TypeUtils' imported from .., closes #26

--- src/historicalfigure/HistoricalFigureComponent.js
+++ src/historicalfigure/HistoricalFigureComponent.js
@@ -1,5 +1,5 @@
 import { Component } from '../ecs/Component.js';
-import { TypeUtils } from '../util/TypeUtils';
+import { TypeUtils } from '../util/TypeUtils.js';
 
 /**
  * @class HistoricalFigureComponent
