cc008b24e93256b6c98d05d8cad6d3c3c0d52789
Allan Thrane Andersen <einheriii@gmail.com>
2025-09-06 18:06:11 +0200

    Clarify if MenuInputSystem also collects sub classes of MenuComponent, closes #218

---
+++ test/ecs/SystemInheritance.test.ts
@@ -0,0 +1,34 @@
+import { EntityManager } from '../../src/ecs/EntityManager';
+import { System } from '../../src/ecs/System';
+import { Component } from '../../src/ecs/Component';
+import { Entity } from '../../src/ecs/Entity';
+
+class C1 extends Component {}
+class C2 extends C1 {}
+
+class S1 extends System {
+  public processed: string[] = [];
+  constructor(entityManager: EntityManager) {
+    super(entityManager, [C1]);
+  }
+   
+  processEntity(entity: Entity, ...args: any[]): void {
+    this.processed.push(entity.getId());
+  }
+}
+
+describe('System component matching with inheritance', () => {
+  it('processes entities with exact component C1 but not entities with subclass C2', () => {
+    const em = EntityManager.create();
+
+    const e1 = em.createEntity(new C1());
+    const e2 = em.createEntity(new C2());
+
+    const s1 = new S1(em);
+    s1.update();
+
+    expect(s1.processed).toContain(e1.getId());
+    expect(s1.processed).not.toContain(e2.getId());
+    expect(s1.processed.length).toBe(1);
+  });
+});
