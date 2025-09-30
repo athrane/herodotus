d91e94f901b7bb785f4bcb993f2991be183f3e7d
Allan Thrane Andersen <einheriii@gmail.com>
2025-09-30 17:16:30 +0200

    Delete class HistoricalFigure, closes #267

--- src/historicalfigure/HistoricalFigure.ts
+++
@@ -1,4 +0,0 @@
-// This module intentionally left blank after migrating HistoricalFigure data into HistoricalFigureComponent.
-// Imports of './HistoricalFigure' should be updated to reference './HistoricalFigureComponent'.
-
-export {};
